11bd88629dc929f815dca72740ea404398ffddaf
Harm Kolvoort <hkolvoort+github@gmail.com>
2016-12-31 16:52:17 +0100

    Initial commit of ng2 heroes tutorial

---
+++ app/app.module.ts
@@ -0,0 +1,38 @@
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import {AppRoutingModule} from "./app-routing.module";
+import {HttpModule} from "@angular/http";
+import {AppComponent} from "./app.component";
+import {HeroDetailComponent} from "./hero-detail.component";
+import {HeroesComponent} from "./heroes.component";
+import {HeroService} from "./hero.service";
+import {DashboardComponent} from "./dashboard.component";
+import {InMemoryWebApiModule} from "angular-in-memory-web-api";
+import {InMemoryDataService} from "./in-memory-data.service";
+import "./rxjs-extensions";
+import {HeroSearchComponent} from "./hero-search.component";
+
+
+@NgModule({
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    ],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent,
+    HeroSearchComponent,
+  ],
+  providers:    [
+    HeroService,
+  ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
+
---
+++ app/in-memory-data.service.ts
@@ -0,0 +1,31 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    let heroes = [
+      {id: 11, name: 'Mr. Nice'},
+      {id: 12, name: 'Narco'},
+      {id: 13, name: 'Bombasto'},
+      {id: 14, name: 'Celeritas'},
+      {id: 15, name: 'Magneta'},
+      {id: 16, name: 'RubberMan'},
+      {id: 17, name: 'Dynama'},
+      {id: 18, name: 'Dr IQ'},
+      {id: 19, name: 'Magma'},
+      {id: 20, name: 'Tornado'}
+    ];
+    return {heroes};
+  }
+
+  createVerlofRechten() {
+    let balance = {
+      id: 1,
+      userId: 1,
+      holidays: 200,
+      extraLeave: 104,
+      bought: 64,
+      compensation: 0,
+      leaveTaken: 0
+    };
+    return {balance};
+  }
+}
