102c072f9257914de9234e904be91d1303e31be4
Harm Kolvoort <hkolvoort+github@gmail.com>
2017-01-08 21:38:16 +0100

    Update UI for promise test

--- app/profile.component.ts
+++ app/profile.component.ts
@@ -12,15 +12,41 @@ import {subscribeToResult} from "rxjs/util/subscribeToResult";
 export class ProfileComponent {
   private profile: any;
 
+  number1: any;
+  number2: any;
   result: any;
   error: any;
   time: any;
+  delay = 200;
 
   constructor(private auth: Auth) {
     this.profile = JSON.parse(localStorage.getItem('profile'));
+    this.doStuff(this.number1 = 5, this.number2 = 2);
+  }
 
+  public add(x: number,y: number): Promise<number>{
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const result = x + y;
+        if (result >= 0){
+          resolve(result);
+        } else {
+          reject('invalid value :' + result);
+        }
+      }, Number(this.delay));
+    });
+  }
+
+  public initialise(){
+    this.result = null;
+    this.error = null;
+    this.time = null;
+  }
+
+  public doStuff(x: number, y: number) {
+    this.initialise();
     const startTime = Date.now();
-    this.add(5,2)
+    this.add(Number(x),Number(y))
       .then(result => this.add(result, 3))
       .then(result => this.add(result, 1))
       .then(result => {
@@ -31,16 +57,4 @@ export class ProfileComponent {
       .then(() => this.time = Date.now() - startTime);
   }
 
-  add(x: number,y: number): Promise<number>{
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const result = x + y;
-        if (result >= 0){
-          resolve(result);
-        } else {
-          reject('invalid value :' + result);
-        }
-      }, 100);
-    });
-  }
 }
