ea3c355583b47d1b458f08554f1016257bdd9409
Harm Kolvoort <hkolvoort+github@gmail.com>
2017-01-28 23:47:48 +0100

    decoding of JWT header and payload added

--- app/jwt.component.ts
+++ app/jwt.component.ts
@@ -1,5 +1,6 @@
 import {Auth} from "./auth.service";
 import {Component} from "@angular/core";
+import {JwtHelper} from 'angular2-jwt';
 
 @Component({
   moduleId: module.id,
@@ -10,16 +11,25 @@ import {Component} from "@angular/core";
 
 export class JwtComponent {
   private token : any;
+  private tokenHeader: any;
   private decodedToken: any;
+  private tokenExpirationDate: any;
+  private isTokenExpired: boolean;
+
+  private jwtHelper: JwtHelper = new JwtHelper();
 
   constructor(private auth: Auth){
     this.token = localStorage.getItem('id_token');
-    this.decodedToken = this.parseJwt(this.token);
+    this.tokenHeader = JSON.parse(this.jwtHelper.urlBase64Decode(this.token.split('.')[0]));
+    this.decodedToken = this.jwtHelper.decodeToken(this.token);
+    // this.decodedToken = JSON.parse(this.jwtHelper.urlBase64Decode(this.token.split('.')[1]));
+    this.tokenExpirationDate = this.jwtHelper.getTokenExpirationDate(this.token);
+    this.isTokenExpired = this.jwtHelper.isTokenExpired(this.token);
   }
 
   parseJwt (token: any) {
   var base64Url = token.split('.')[1];
   var base64 = base64Url.replace('-', '+').replace('_', '/');
   return JSON.parse(window.atob(base64));
-  };
+  }
 }
