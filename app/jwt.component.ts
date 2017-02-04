9bfd16fdf0d01b99cfc7ecd26d91ebb2a60d6469
Harm Kolvoort <hkolvoort+github@gmail.com>
2017-02-04 16:07:19 +0100

    big step forward with verifying jwt signature

--- app/jwt.component.ts
+++ app/jwt.component.ts
@@ -5,9 +5,10 @@ import {JwtHelper} from 'angular2-jwt';
 @Component({
   moduleId: module.id,
   selector: 'jwt',
-  providers: [Auth],
   templateUrl: 'jwt.component.html',
 })
+
+
 
 export class JwtComponent {
   private token : any;
@@ -20,11 +21,12 @@ export class JwtComponent {
 
   constructor(private auth: Auth){
     this.token = localStorage.getItem('id_token');
+
     this.tokenHeader = JSON.parse(this.jwtHelper.urlBase64Decode(this.token.split('.')[0]));
     this.decodedToken = this.jwtHelper.decodeToken(this.token);
-    // this.decodedToken = JSON.parse(this.jwtHelper.urlBase64Decode(this.token.split('.')[1]));
     this.tokenExpirationDate = this.jwtHelper.getTokenExpirationDate(this.token);
     this.isTokenExpired = this.jwtHelper.isTokenExpired(this.token);
+
     this.verifyToken(this.token);
   }
 
@@ -32,25 +34,13 @@ export class JwtComponent {
     var header = token.split('.')[0];
     var payload = token.split('.')[1];
     var signature = token.split('.')[2];
-    console.log(token);
-    console.log(header);
-    console.log(payload);
-
-    var tokenWithoutSignature = header + '.' +  payload;
-    console.log(tokenWithoutSignature);
-
     var cryptoJS = require("../node_modules/crypto-js/crypto-js.js");
-    console.log(atob(cryptoJS.HmacSHA256(tokenWithoutSignature, "rBbZZbFKPpk-Hu4hnv1lBwQxkropr_U4aLyZUEdyUyFtjl02lR9o1Og4cAaQsRNJ")));
-    // var correctSignature = cryptoJS.HmacSHA256(tokenWithoutSignature);
 
-    console.log(signature);
-    // console.log(correctSignature);
-
+    console.log('token = ' + token);
+    console.log('header = ' + header);
+    console.log('payload = ' + payload);
+    console.log('original signature = ' + signature);
+    console.log('new signature = ' + cryptoJS.HmacSHA256(header + '.' + payload , "rBbZZbFKPpk-Hu4hnv1lBwQxkropr_U4aLyZUEdyUyFtjl02lR9o1Og4cAaQsRNJ").toString(cryptoJS.enc.Base64));
   }
 
-  parseJwt (token: any) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace('-', '+').replace('_', '/');
-  return JSON.parse(window.atob(base64));
-  }
 }
