03899cc9da5e2382df7a4abe1dc4753e283504d5
harm kolvoort <hkolvoort+github@gmail.com>
2017-02-04 17:25:13 +0100

    verifyToken working fine with some replace/regex

--- app/jwt.component.ts
+++ app/jwt.component.ts
@@ -20,27 +20,38 @@ export class JwtComponent {
   private jwtHelper: JwtHelper = new JwtHelper();
 
   constructor(private auth: Auth){
-    this.token = localStorage.getItem('id_token');
+    if (this.token = localStorage.getItem('id_token')){
+      this.tokenHeader = JSON.parse(this.jwtHelper.urlBase64Decode(this.token.split('.')[0]));
+      this.decodedToken = this.jwtHelper.decodeToken(this.token);
+      this.tokenExpirationDate = this.jwtHelper.getTokenExpirationDate(this.token);
+      this.isTokenExpired = this.jwtHelper.isTokenExpired(this.token);
 
-    this.tokenHeader = JSON.parse(this.jwtHelper.urlBase64Decode(this.token.split('.')[0]));
-    this.decodedToken = this.jwtHelper.decodeToken(this.token);
-    this.tokenExpirationDate = this.jwtHelper.getTokenExpirationDate(this.token);
-    this.isTokenExpired = this.jwtHelper.isTokenExpired(this.token);
-
-    this.verifyToken(this.token);
+      this.verifyToken(this.token);
+    };
   }
 
   verifyToken(token: any){
+    var cryptoJS = require("../node_modules/crypto-js/crypto-js.js");
+
     var header = token.split('.')[0];
     var payload = token.split('.')[1];
     var signature = token.split('.')[2];
-    var cryptoJS = require("../node_modules/crypto-js/crypto-js.js");
+
+    var receivedSignature =
+      cryptoJS.HmacSHA256(
+          header + '.' + payload,
+          "rBbZZbFKPpk-Hu4hnv1lBwQxkropr_U4aLyZUEdyUyFtjl02lR9o1Og4cAaQsRNJ"
+        )
+        .toString(cryptoJS.enc.Base64)
+        .replace(/\//g,"_").replace(/\+/g,"-").replace(/=+$/g,"")
+      ;
+
 
     console.log('token = ' + token);
     console.log('header = ' + header);
     console.log('payload = ' + payload);
     console.log('original signature = ' + signature);
-    console.log('new signature = ' + cryptoJS.HmacSHA256(header + '.' + payload , "rBbZZbFKPpk-Hu4hnv1lBwQxkropr_U4aLyZUEdyUyFtjl02lR9o1Og4cAaQsRNJ").toString(cryptoJS.enc.Base64));
+    console.log('new signature = ' + receivedSignature);
   }
 
 }
