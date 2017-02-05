949240346c58d605f3c4acb01d0002232786aa23
Harm Kolvoort <hkolvoort+github@gmail.com>
2017-02-05 10:10:31 +0100

    added text coloring inspired by jwt.io

--- app/jwt.component.ts
+++ app/jwt.component.ts
@@ -13,20 +13,24 @@ import {JwtHelper} from 'angular2-jwt';
 
 export class JwtComponent {
   private token : any;
-  private tokenHeader: any;
-  private decodedToken: any;
+  private header: any;
+  private headerJSON: any;
+  private payload: any;
+  private payloadJSON: any;
+  private signature: any;
   private tokenExpirationDate: any;
   private isTokenExpired: boolean;
   private isValid: boolean;
-  private signature: any;
   private receivedSignature: any;
 
   private jwtHelper: JwtHelper = new JwtHelper();
 
   constructor(private auth: Auth){
     if (this.token = localStorage.getItem('id_token')){
-      this.tokenHeader = JSON.parse(this.jwtHelper.urlBase64Decode(this.token.split('.')[0]));
-      this.decodedToken = this.jwtHelper.decodeToken(this.token);
+      this.header = this.token.split('.')[0];
+      this.headerJSON = JSON.parse(this.jwtHelper.urlBase64Decode(this.header));
+      this.payload = this.token.split('.')[1];
+      this.payloadJSON = JSON.parse(this.jwtHelper.urlBase64Decode(this.payload));
       this.signature = this.token.split('.')[2];
       this.tokenExpirationDate = this.jwtHelper.getTokenExpirationDate(this.token);
       this.isTokenExpired = this.jwtHelper.isTokenExpired(this.token);
